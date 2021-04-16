ea96fddcba353ad73034e3db20589962f15e1b9d
Mark Grebe <grebe@ucmo.edu>
2021-04-16 12:02:50 -0500

    Changed DB to PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_04_09_210521) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cars", force: :cascade do |t|
     t.string "make"
     t.string "model"
